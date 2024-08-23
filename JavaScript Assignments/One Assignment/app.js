42e31be5c1561c6bc75fbd17c893bd6fafffdac0
Saeed Ali <saeedaliofficial77@gmail.com>
2024-08-23 22:50:11 +0500

    Assignment One

---
+++ JavaScript Assignments/One Assignment/app.js
@@ -0,0 +1,21 @@
+
+// ## Variables #1
+
+// 1. create "firstName" and "last_name" variabl
+// 2. assign your values
+
+// 3. create "address" variable and assign "main
+// street" value to it
+
+// 4. re-assign address to "first street" later
+// 5. log all values in the console
+
+var firstName = "Saeed";
+
+var last_name = "Ali";
+
+var address = "Main Street";
+
+address = "First Street";
+
+console.log(firstName,last_name,address);
